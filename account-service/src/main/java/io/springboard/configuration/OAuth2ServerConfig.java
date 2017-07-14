02a3d2c8fb8e7b14d9ceeb665168bc5ccd51e2e1
fanjun <fanj@yowits.com>
2017-07-14 11:38:22 +0800

    ResourceServer根据token生成SecUser

--- account-service/src/main/java/io/springboard/configuration/OAuth2ServerConfig.java
+++ account-service/src/main/java/io/springboard/configuration/OAuth2ServerConfig.java
@@ -1,11 +1,24 @@
 package io.springboard.configuration;
 
+import io.springboard.framework.security.SecUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
 public class OAuth2ServerConfig {
@@ -16,9 +29,15 @@ public class OAuth2ServerConfig {
 	@EnableResourceServer
 	protected static class DevResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
+		@Qualifier("userInfoTokenServices")
+		@Autowired
+		UserInfoTokenServices userInfoTokenServices;
+
 		@Override
 		public void configure(ResourceServerSecurityConfigurer resources) {
 			resources.resourceId(RESOURCE_ID);
+			// 根据userinfo生成Principle
+			userInfoTokenServices.setPrincipalExtractor(new BanbuPrincipalExtractor());
 		}
 
 		@Override
@@ -61,4 +80,26 @@ public class OAuth2ServerConfig {
 			// @formatter:on
 		}
 	}
+}
+
+class BanbuPrincipalExtractor implements PrincipalExtractor {
+	@Override
+	public Object extractPrincipal(Map<String, Object> map) {
+		Map<String, Object> principal = (Map<String, Object>)map.get("principal");
+		Long userID = Long.valueOf(principal.get("userID").toString());
+		String fullName = (String)principal.get("fullName");
+		String username = (String)principal.get("username");
+		boolean enabled = (boolean)principal.get("enabled");
+		boolean credentialsNonExpired = (boolean)principal.get("credentialsNonExpired");
+		boolean accountNonLocked = (boolean)principal.get("accountNonLocked");
+		boolean accountNonExpired = (boolean)principal.get("accountNonExpired");
+		List<Map<String,String>> authorities = (List<Map<String,String>>)principal.get("authorities");
+		Collection<GrantedAuthority> grantedAuths = authorities.stream().map(item -> new SimpleGrantedAuthority(item.get("authority")))
+				.collect(Collectors.toList());
+
+		SecUser user = new SecUser(userID, username, "", fullName,
+				enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, grantedAuths);
+		return user;
+	}
+
 }
